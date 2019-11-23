8ba992f3688504f4a5ec45edae1d0ac572db6f01
Richard <rlii@uwo.ca>
2019-11-23 18:33:11 -0500

    Clean up files, finalized AboutForm format

--- frontend/src/Components/AboutForm.js
+++ frontend/src/Components/AboutForm.js
@@ -14,8 +14,8 @@ function AboutForm() {
       <h1 style={{textAlign: "center"}}> About Us</h1>
         
           <Row>
-            <Col  className ="aboutText" style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={jesus} roundedCircle></Image></Col>
-            <Col>
+            <Col style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={jesus} roundedCircle></Image></Col>
+            <Col className ="aboutText" >
               <p style ={{textAlign:"center"}}>
                 <b>Name:</b> Jesus Garcia Moreno<br></br>
                 <b>School:</b> University of Waterloo<br></br>
@@ -23,31 +23,31 @@ function AboutForm() {
                 <b>Year:</b> Sophmore
               </p>
             </Col>
-            <Col className ="aboutText" style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={richard} roundedCircle></Image></Col>
-            <Col>
+            <Col style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={richard} roundedCircle></Image></Col>
+            <Col className ="aboutText" >
               <p>
               <b>Name:</b> Richard Lii<br></br>
-              <b>School:</b> University of Western Ontario<br></br>
+              <b>School:</b> Western University<br></br>
               <b>Program:</b> Software Engineering<br></br>
               <b>Year:</b> Sophmore 
               </p>
             </Col>
           </Row>
           <Row>
-            <Col className ="aboutText" style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={arsh} roundedCircle></Image></Col>
-            <Col>
+            <Col style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={arsh} roundedCircle></Image></Col>
+            <Col className ="aboutText">
               <p>
               <b>Name:</b> Arsh Lalani<br></br>
-              <b>School:</b> University of Western Ontario<br></br>
+              <b>School:</b> Western University<br></br>
               <b>Program:</b> Software Engineering<br></br>
               <b>Year:</b> Sophmore 
               </p>
             </Col>
-            <Col className ="aboutText" style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={aasir} roundedCircle></Image></Col>
-            <Col>
+            <Col style = {{textAlign:"center"}}> <Image className="w-100 p-2" src ={aasir} roundedCircle></Image></Col>
+            <Col className ="aboutText">
               <p>
               <b>Name:</b> Aasir Valji<br></br>
-              <b>School:</b> University of Western Ontario<br></br>
+              <b>School:</b> Western University<br></br>
               <b>Program:</b> Software Engineering<br></br>
               <b>Year:</b> Sophmore 
               </p>
