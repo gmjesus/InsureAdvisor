62ce741b538c567b302de4a2eefdfb94bc12e01b
av <45238682+avxlji@users.noreply.github.com>
2019-11-23 13:15:42 -0500

    Added navbar and parrallax background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import UserForm from './Components/UserForm'
+import Navigate from './Components/Navigate';
 
 
 
 function App() {
   return (
-    <UserForm />
+    <>
+      <Navigate className='sample'/>
+
+
+<h1 className='intro-text'> Welcome to InsureAdvisure</h1>
+<div className='user-form'>
+
+  <UserForm className='uForm'></UserForm>
+  
+</div>
+    </>
   );
 }
 
