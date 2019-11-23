e6b8371d80ce7f91815c8228fc55070de0f73a4d
Jesus Garcia Moreno <jesus2208f@gmail.com>
2019-11-23 17:32:43 -0500

    Linked frontend and backend in user form

--- frontend/src/Components/UserForm.js
+++ frontend/src/Components/UserForm.js
@@ -1,20 +1,67 @@
 import React from 'react';
 import '../App.css';
-import { Card, Button, Form, Container} from 'react-bootstrap';
+import { Card, Button, Form, Container, InputGroup} from 'react-bootstrap';
+import axios from 'axios';
 
 function UserForm() {
   const [name, setName] = React.useState();
   const [email, setEmail] = React.useState();
   const [age, setAge] = React.useState();
-  const [type, setType] = React.useState();
+  const [country, setCountry] = React.useState("Canada");
+  const [propertyInsurance, setPropertyInsurance] = React.useState(false);
+  const [lifeInsurance, setLifeInsurance] = React.useState(false);
+  const [carInsurance, setCarInsurance] = React.useState(false);
+  const [healthInsurance, setHealthInsurance] = React.useState(false);
+  const [disabilityInsurance, setDisabilityInsurance] = React.useState(false);
+  const [fireInsurance, setFireInsurance] = React.useState(false);
+  const [socialInsurance, setSocialInsurance] = React.useState(false);
+  const [otherInsurance, setOtherInsurance] = React.useState(false);
+  const [maxLimit, setMaxLimit] = React.useState();
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    var types = [];
+    if(propertyInsurance){
+      types.push("Property");
+    }
+    if (lifeInsurance) {
+      types.push("Life");
+    }
+    if (carInsurance) {
+      types.push("Car");
+    }
+    if (healthInsurance) {
+      types.push("Health and Dental");
+    } 
+    if (disabilityInsurance) {
+      types.push("Disability");
+    } 
+    if (fireInsurance) {
+      types.push("Fire");
+    } 
+    if (socialInsurance) {
+      types.push("Social");
+    } 
+    if (otherInsurance) {
+      types.push("Other");
+    }
 
+    async function postData(){
+      await axios({
+        method: 'post',
+        url: "http://localhost:3001/users",
+        data: {
+          Name: name,
+          Email: email,
+          Age: age,
+          Citizenship: country,
+          InsuranceTypes: types,
+          MaxLimit: maxLimit
+        }
+      });
+    }
+    postData();
   }
-  console.log(name);
-  console.log(email);
-  console.log(age);
 
   return (
     <Container>
@@ -36,14 +83,37 @@ function UserForm() {
             <Form.Control required type="age" placeholder="Enter Age" onChange={age => setAge(age.target.value)}/>
           </Form.Group>
 
-          <Form.Label>What types of insurance are you looking for?</Form.Label>
-          {['Property', 'Life', 'Car', 'Health and Dental', 'Fire', 'Social'].map(type => (
-              <Form.Check 
-                key={type}
-                label={`${type} Insurance`}
-              />
-          ))}
+          <Form.Group>
+            <Form.Label>Country of Residency</Form.Label>
+            <Form.Control as="select" onChange={country => setCountry(country.target.value)}>
+              <option>Canada</option>
+              <option>United States</option>
+            </Form.Control>
+          </Form.Group>
 
+          <Form.Group>
+            <Form.Label>What types of insurance are you looking for?</Form.Label>
+            <Form.Check type="checkbox" label="Property Insurance" onChange={propertyInsurance => setPropertyInsurance(propertyInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Life Insurance" onChange={lifeInsurance => setLifeInsurance(lifeInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Car Insurance" onChange={carInsurance => setCarInsurance(carInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Health and Dental Insurance" onChange={healthInsurance => setHealthInsurance(healthInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Disability Insurance" onChange={disabilityInsurance => setDisabilityInsurance(disabilityInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Fire Insurance" onChange={fireInsurance => setFireInsurance(fireInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Social Insurance" onChange={socialInsurance => setSocialInsurance(socialInsurance.target.checked)}/>
+            <Form.Check type="checkbox" label="Other" onChange={otherInsurance => setOtherInsurance(otherInsurance.target.checked)}/>
+          </Form.Group>
+
+          <Form.Group controlId="max">
+            <Form.Label>Highest Acceptable Quotes</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text>$</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control required type="max" placeholder="Enter Maximum Monthly Limit" onChange={max => setMaxLimit(max.target.value)}/>
+            </InputGroup>
+          </Form.Group>
+
+          <hr />
           <Button variant="primary" type="submit">Submit</Button>
 
         </Form>
