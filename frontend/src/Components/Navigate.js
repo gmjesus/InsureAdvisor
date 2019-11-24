72d0915ca35b972468985f9c10b5ae25f5de710f
Richard <rlii@uwo.ca>
2019-11-23 21:48:26 -0500

    removed search, Formated Nav

--- frontend/src/Components/Navigate.js
+++ frontend/src/Components/Navigate.js
@@ -15,14 +15,10 @@ function Navigate() {
         InsuranceAdvisor</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="ml-auto">
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="light">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     </Container>
